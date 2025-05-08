4d44d4303fe1d056c7d596928684ccb0b69203cf
Aayush Joshi <aayushjoshi129@gmail.com>
2025-05-08 20:30:53 +0530

    uber-project backend service for user done

---
+++ uber_project/Backend/models/blackListToken.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const blackListTokenSchema = new mongoose.Schema({
+    token: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, expires: 86400 } // TTL of 24 hours (86400 seconds)
+});
+
+module.exports = mongoose.model('BlackListToken', blackListTokenSchema);
