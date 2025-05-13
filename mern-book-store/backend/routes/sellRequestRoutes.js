7bdc822c485daabc6a0a0972a8f75dc7c79ef6a4
Aayush Joshi <aayushjoshi129@gmail.com>
2025-05-13 19:01:10 +0530

    deleted mern_book_store project

--- mern-book-store/backend/routes/sellRequestRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { submitSellRequest, getSellRequests } = require('../controllers/sellRequestController');
-const { protect } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.route('/').post(protect, submitSellRequest).get(protect, getSellRequests);
-
-module.exports = router;
